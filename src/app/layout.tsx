8448a08547d806db506a4ab36ef5097fd3213cd7
Asad Ali <Asad Ali>
2024-12-10 10:30:13 +0500

    fsv icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet" />
         <title>Asad's Portfolio</title>
+        <link rel="icon" type="image/png" href="/fav-icon.png" />
       </head>
 
       <body
